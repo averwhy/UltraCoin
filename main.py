9edb59d3facf00f244dac13a7764b58cbb5413aa
averyB <averwhy@gmail.com>
2019-11-13 14:21:23 -0500

    commiting so i can go home

--- main.py
+++ main.py
@@ -9,13 +9,52 @@ import sys
 #  MAY NOT BE FUNCTIONAL! THIS CODE IS ONLY ON GITHUB SO MY GROUP MEMBERS CAN ACCESS IT.    #
 #############################################################################################
 
+global coin
+global balance
+global debugMode
+global build
 coin = False #using a boolean is easier, so: False = Tails, and True = Heads
-debugMode = False #cool feature i want to add. False by default
-balance = 5 # Your money. Default is 5.
+debugMode = False # False by default, deprecated at the moment.
+balance = 10 # Your money. Default is 10.
+build = 1 # build information since this will be compiled to an exe.
+
+def heads():
+    print("Since you got heads, you can 1 dice.")
+    one_dice = input("Press ENTER to roll dice.")
+    dice_random = random.randint(1,6)
+    print("Your dice rolled a",dice_random)
+
+def tails():
+    print("Since you got heads, you can 2 die.")
+    two_dice = input("Press ENTER to roll dice.")
+    dice_random = random.randint(1,6)
+    dice_random_2 = random.randint(1,6)
+    print("Dice 1 rolled a",dice_random,", and dice 2 rolled a",dice_random_2,".")
+
+def main():
+    print("Welcome Ultra Coin. Please chose from the following:")
+    print("1. Play")
+    print("2. Options")
+    print("")
+    print("Your money: ",balance)
+    ask1 = input(">")
+
+    if ask1 == "1": 
+        play()
+    
+# def dev_mode():
+#     if debugMode == False:
+#         debugMode = True
+#     elif debugMode == True:
+#         debugMode = False
+#     print("")
+#     print("Developer mode enabled.")
+#     print("")
+#     main()
 
 def play(): # This is the play process
-    if debugMode == True:
-        print("")
+    # if debugMode == True:
+    #     print("")
     print("You have chosen [Play].")
     print("")
     print("")
@@ -24,25 +63,12 @@ def play(): # This is the play process
     if coin_random > 50:
         coin = False # Tails
         print("Tails!")
+        tails()
     else:
         coin = True # Heads
         print("Heads!")
+        heads()
     
     print("")
 
-def main():
-    print("Welcome Ultra Coin. Please chose from the following:")
-    print("1. Play")
-    print("2. Options")
-    print("")
-    ask1 = input(">")
-
-    if ask1 == "1": 
-        play()
-    
-def dev_mode():
-    debugMode = True
-    print("")
-    print("Developer mode enabled.")
-    print("")
-    main()
+main()
